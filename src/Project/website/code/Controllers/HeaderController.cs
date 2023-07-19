e414548d7a6f48ca090d4a2fd623e4936c4f9e50
rajendersudikay <rajender.sudikay@gmail.com>
2023-07-19 13:03:12 +0530

    - Implented Header component with controller

---
+++ src/Project/website/code/Controllers/HeaderController.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static Sitecore.Shell.UserOptions.HtmlEditor;
+
+namespace MedProSC.Project.Website.Controllers
+{
+    public class HeaderController : Controller
+    {
+        // GET: Header
+        public ActionResult Header()
+        {
+            return View("~/Views/Shared/Header.cshtml");
+        }
+    }
+}
