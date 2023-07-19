fc365bc4f9efbcc3a3a41edd3408b15513878eca
rajendersudikay <rajender.sudikay@gmail.com>
2023-07-19 16:06:10 +0530

    - Removed app start folder

--- src/Project/website/code/Controllers/HeaderController.cs
+++ src/Project/website/code/Controllers/HeaderController.cs
@@ -1,32 +1,26 @@
-﻿using Sitecore.IO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using static Sitecore.Shell.UserOptions.HtmlEditor;
-using Sitecore.Mvc;
+﻿using MedProSC.Project.Website.Models;
 using Sitecore.Mvc.Presentation;
-using MedProSC.Project.Website.Models;
+using System.Web.Mvc;
 
 namespace MedProSC.Project.Website.Controllers
 {
+    using static Templates;
+
     public class HeaderController : Controller
-    {
-        // GET: Header
+    {       
         public ActionResult Header()
         {
             var headerModel = new HeaderModel();
             var headerDataSource = RenderingContext.Current.Rendering.DataSource;
-            if(!string.IsNullOrWhiteSpace( headerDataSource))
+            if (!string.IsNullOrWhiteSpace(headerDataSource))
             {
                 var headerDataSoureItem = Sitecore.Context.Database.GetItem(headerDataSource, Sitecore.Context.Language);
-            
-                if(headerDataSoureItem != null)
+
+                if (headerDataSoureItem != null)
                 {
-                    headerModel.HeaderTitle = headerDataSoureItem.Fields["HeaderTitle"].Value;
-                    headerModel.FormBuilderTitle = headerDataSoureItem.Fields["FormBuilderTitle"].Value;
-                    headerModel.BundleHistoryTitle = headerDataSoureItem.Fields["BundleHistoryTitle"].Value;
+                    headerModel.HeaderTitle = headerDataSoureItem.Fields[HeaderTemplate.Fields.HeaderTitle].Value;
+                    headerModel.FormBuilderTitle = headerDataSoureItem.Fields[HeaderTemplate.Fields.FormBuilderTitle].Value;
+                    headerModel.BundleHistoryTitle = headerDataSoureItem.Fields[HeaderTemplate.Fields.BundleHistoryTitle].Value;
                 }
             }
             return View("~/Views/Shared/Header.cshtml", headerModel);
---
+++ src/Project/website/code/Templates.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Data;
+
+namespace MedProSC.Project.Website
+{
+    public class Templates
+    {
+        public struct HeaderTemplate
+        {
+            public static readonly ID TemplateID = new ID("{56716662-0509-45AA-91A4-C1352ABA90B8}");
+
+            public struct Fields
+            {
+                public static readonly ID HeaderTitle = new ID("{108CDDAB-5261-42BA-A76A-6E9A20E40143}");
+                public static readonly ID FormBuilderTitle = new ID("{E4289044-FB46-4472-877B-6E001E579B17}");
+                public static readonly ID BundleHistoryTitle = new ID("{F7F28687-C70C-41B5-9AA5-81B9EDFBB856}");
+            }
+        }
+    }
+}
