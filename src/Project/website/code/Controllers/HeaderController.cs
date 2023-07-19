95489760357888876a3929046443420556b1534c
rajendersudikay <rajender.sudikay@gmail.com>
2023-07-19 14:06:08 +0530

    - Header dynamic logic implemented

--- src/Project/website/code/Controllers/HeaderController.cs
+++ src/Project/website/code/Controllers/HeaderController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using static Sitecore.Shell.UserOptions.HtmlEditor;
+using Sitecore.Mvc;
+using Sitecore.Mvc.Presentation;
+using MedProSC.Project.Website.Models;
 
 namespace MedProSC.Project.Website.Controllers
 {
@@ -13,7 +16,20 @@ namespace MedProSC.Project.Website.Controllers
         // GET: Header
         public ActionResult Header()
         {
-            return View("~/Views/Shared/Header.cshtml");
+            var headerModel = new HeaderModel();
+            var headerDataSource = RenderingContext.Current.Rendering.DataSource;
+            if(!string.IsNullOrWhiteSpace( headerDataSource))
+            {
+                var headerDataSoureItem = Sitecore.Context.Database.GetItem(headerDataSource, Sitecore.Context.Language);
+            
+                if(headerDataSoureItem != null)
+                {
+                    headerModel.HeaderTitle = headerDataSoureItem.Fields["HeaderTitle"].Value;
+                    headerModel.FormBuilderTitle = headerDataSoureItem.Fields["FormBuilderTitle"].Value;
+                    headerModel.BundleHistoryTitle = headerDataSoureItem.Fields["BundleHistoryTitle"].Value;
+                }
+            }
+            return View("~/Views/Shared/Header.cshtml", headerModel);
         }
     }
 }
---
+++ src/Project/website/code/Models/HeaderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MedProSC.Project.Website.Models
+{
+    public class HeaderModel
+    {
+        public string HeaderTitle { get; set; }
+
+        public string FormBuilderTitle { get; set; }
+
+        public string BundleHistoryTitle { get; set; }
+    }
+}
