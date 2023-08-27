841bc8013f77a51c1755e4427c26f339ca69aeec
rajendersudikay <rajender.sudikay@gmail.com>
2023-08-27 15:09:10 +0530

    - My Bundle css and HTML updates

--- src/Project/website/code/Assets/scripts/buildertable.js
+++ src/Project/website/code/Assets/scripts/buildertable.js
@@ -1,3 +1,17 @@
+
+for (let i = 0; i < 10; i++) {    
+    var markup = "<tr>"
+        + " <td class='formNumber'></td >"
+        + " <td class='formName'></td > "
+        + " <td class='occurence'></td >"
+        + " <td class='claimsmade'></td >"
+        + " <td class='effectiveDate'></td >"
+        + " <td class='expirationDate'></td >"
+        + " <td class='action' ></td >"
+        + "</tr>";
+    jQuery(".form-tablebody").append(markup);
+}
+
 jQuery(".btn-load-form").click(function () {
     var settings = {
         "url": "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json",
