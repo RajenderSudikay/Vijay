95fdf91fe92f0e298240913805f222962d07cc07
rajendersudikay <rajender.sudikay@gmail.com>
2023-10-04 19:59:56 +0530

    - OKTA working solution with Medpro end points

--- src/Foundation/Security/code/Constants.cs
+++ src/Foundation/Security/code/Constants.cs
@@ -7,7 +7,7 @@
             public static readonly string IdToken = "id_token";
             public static readonly string IdentityProvider = "Okta";
             public static readonly string OpenIdEmail = " email";
-            public static readonly string AccessDeniedRelativePath = "/manualforms/denied";
+            public static readonly string AccessDeniedRelativePath = "/access-denied";
             public static readonly string ClaimType = "name";
             public static readonly string RequireNonceDeaultValue = "IDX21323";
         }
